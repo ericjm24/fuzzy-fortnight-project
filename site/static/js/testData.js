adc837917a64decda34dc4ee689ba546747b8523
Eric <ericjm24@gmail.com>
2019-10-23 14:47:30 -0700

    added testData

---
+++ site/static/js/testData.js
@@ -0,0 +1,26 @@
+var testData = {
+    userID: 75,
+    color:"blue",
+    friendsList:[12, 16, 19, 42, 85, 193, 199, 240, 4156],
+    followersList:[12, 16, 23, 25, 28, 32, 36, 76, 77, 85, 123, 145, 167, 187, 188, 198, 240, 256, 543, 842, 1853, 2983],
+    connectionsList:[
+        {userID:16,
+        weight:7,
+        color:"blue"},
+        {userID:23,
+        weight:3,
+        color:"blue"},
+        {userID:54,
+        weight:13,
+        color:"red"},
+        {userID:79,
+        weight:2,
+        color:"red"},
+        {userID:145,
+        weight:5,
+        color:"red"},
+        {userID:198,
+        weight:6,
+        color:"grey"}
+    ]
+}
